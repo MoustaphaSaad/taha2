32a97a9407e3dd17fc30f8b1613afe2c9b89a336
MoustaphaSaad <moustapha.saad.abdelhamed@gmail.com>
2023-06-11 23:08:09 +0300

    add unit test allocator

---
+++ package-lock.cmake
@@ -0,0 +1,11 @@
+# CPM Package Lock
+# This file should be committed to version control
+
+# doctest
+CPMDeclarePackage(doctest
+  NAME doctest
+  GIT_TAG v2.4.11
+  GIT_REPOSITORY git@github.com:doctest/doctest.git
+  GIT_SHALLOW TRUE
+  EXCLUDE_FROM_ALL TRUE
+)
