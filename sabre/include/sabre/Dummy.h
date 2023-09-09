d2857ccbdf7752e90485eb0ccd1bd2dec43757cb
MoustaphaSaad <moustapha.saad.abdelhamed@gmail.com>
2023-09-09 20:55:08 +0300

    add sabre target

---
+++ sabre/include/sabre/Dummy.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "sabre/Exports.h"
+
+namespace sabre
+{
+	SABRE_EXPORT int add(int a, int b);
+}
---
+++ sabre/src/sabre/Dummy.cpp
@@ -0,0 +1,9 @@
+#include "sabre/Dummy.h"
+
+namespace sabre
+{
+	int add(int a, int b)
+	{
+		return a + b;
+	}
+}
