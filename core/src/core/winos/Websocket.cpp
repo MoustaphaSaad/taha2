2c37d5c0b97e20d1f40385b42389be04f58f7491
Mostafa Saad <moustapha.saad.abdelhamed@gmail.com>
2023-11-06 23:17:07 +0200

    schedule accept operation

--- core/src/core/winos/Websocket.cpp
+++ core/src/core/winos/Websocket.cpp
@@ -1,9 +1,11 @@
 #include "core/Websocket.h"
+#include "core/Hash.h"
 
 #include <Windows.h>
 
 #include <WinSock2.h>
 #include <WS2tcpip.h>
+#include <Mswsock.h>
 
 #include <assert.h>
 
@@ -11,12 +13,83 @@ namespace core
 {
 	class WinOSServer: public Server
 	{
+		class Op: public OVERLAPPED
+		{
+		protected:
+			enum KIND
+			{
+				KIND_NONE,
+				KIND_ACCEPT,
+			};
+
+			KIND m_kind;
+
+		public:
+			Op(KIND kind)
+				: m_kind(kind)
+			{}
+
+			virtual ~Op() = default;
+		};
+
+		class AcceptOp: public Op
+		{
+			SOCKET m_acceptSocket = INVALID_SOCKET;
+			std::byte m_buffer[2 * (sizeof(SOCKADDR_IN) + 16)];
+		public:
+			AcceptOp()
+				: Op(KIND_ACCEPT)
+			{
+				m_acceptSocket = ::WSASocket(AF_INET, SOCK_STREAM, IPPROTO_TCP, NULL, 0, WSA_FLAG_OVERLAPPED);
+			}
+
+			~AcceptOp() override
+			{
+				if (m_acceptSocket != INVALID_SOCKET)
+				{
+					[[maybe_unused]] auto res = ::closesocket(m_acceptSocket);
+					assert(res == 0);
+				}
+			}
+
+			SOCKET acceptSocket() const
+			{
+				return m_acceptSocket;
+			}
+
+			void* buffer()
+			{
+				return m_buffer;
+			}
+		};
+
+		Allocator* m_allocator = nullptr;
 		HANDLE m_port = INVALID_HANDLE_VALUE;
 		SOCKET m_listenSocket = INVALID_SOCKET;
+		Map<Op*, Unique<Op>> m_scheduledOperations;
+
+		template<typename T>
+		T* getOp()
+		{
+			auto op = core::unique_from<T>(m_allocator);
+			op->hEvent = CreateEvent(NULL, TRUE, FALSE, NULL);
+			auto rawOp = op.get();
+			m_scheduledOperations.insert(rawOp, std::move(op));
+			return rawOp;
+		}
+
+		void putOp(Op* op)
+		{
+			CloseHandle(op->hEvent);
+			m_scheduledOperations.remove(op);
+		}
+
 	public:
-		WinOSServer(HANDLE port, SOCKET listenSocket)
-			: m_port(port),
-			  m_listenSocket(listenSocket)
+		WinOSServer(HANDLE port, SOCKET listenSocket, Allocator* allocator)
+			: m_allocator(allocator),
+			  m_port(port),
+			  m_listenSocket(listenSocket),
+			  m_scheduledOperations(allocator)
 		{}
 
 		~WinOSServer() override
@@ -36,7 +109,34 @@ namespace core
 
 		virtual void run() override
 		{
-			// TODO: Implement this function
+			// schedule an accept operation
+			auto op = getOp<AcceptOp>();
+			// TODO: we can receive on accept operation, it's a little bit more efficient so consider doing that in the future
+			DWORD bytesReceived = 0;
+			auto res = AcceptEx(
+				m_listenSocket,
+				op->acceptSocket(),
+				op->buffer(),
+				0,
+				sizeof(SOCKADDR_IN) + 16,
+				sizeof(SOCKADDR_IN) + 16,
+				&bytesReceived,
+				(OVERLAPPED*)op
+			);
+			if (res == TRUE)
+			{
+				// TODO: handle immediate completion here
+				assert(false);
+			}
+			else
+			{
+				auto error = WSAGetLastError();
+				if (error != ERROR_IO_PENDING)
+				{
+					// TODO: return error of execution here
+					assert(false);
+				}
+			}
 		}
 
 		virtual void stop() override
@@ -53,6 +153,6 @@ namespace core
 		auto listenSocket = ::WSASocket(AF_INET, SOCK_STREAM, IPPROTO_TCP, NULL, 0, WSA_FLAG_OVERLAPPED);
 		if (listenSocket == INVALID_SOCKET) return nullptr;
 
-		return core::unique_from<WinOSServer>(allocator, port, listenSocket);
+		return core::unique_from<WinOSServer>(allocator, port, listenSocket, allocator);
 	}
 }
