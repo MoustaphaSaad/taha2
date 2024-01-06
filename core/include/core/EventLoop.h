edab47e0cd2a1a0e1c00a2fca7ee6eb3a71698e8
MoustaphaSaad <moustapha.saad.abdelhamed@gmail.com>
2024-01-06 06:42:49 +0200

    setup the main workflow of event loop

--- core/include/core/EventLoop.h
+++ core/include/core/EventLoop.h
@@ -11,6 +11,8 @@ namespace core
 	public:
 		CORE_EXPORT static Result<Unique<EventLoop>> create(Allocator* allocator);
 
+		virtual ~EventLoop() = default;
+
 		virtual HumanError run() = 0;
 		virtual void stop() = 0;
 	};
--- core/src/core/winos/EventLoop.cpp
+++ core/src/core/winos/EventLoop.cpp
@@ -1,4 +1,5 @@
 #include "core/EventLoop.h"
+#include "core/Hash.h"
 
 #include <Windows.h>
 
@@ -6,23 +7,112 @@ namespace core
 {
 	class WinOSEventLoop: public EventLoop
 	{
+		struct Op: OVERLAPPED
+		{
+			enum KIND
+			{
+				KIND_NONE,
+				KIND_CLOSE
+			};
+
+			explicit Op(KIND kind_)
+				: kind(kind_)
+			{
+				::memset((OVERLAPPED*)this, 0, sizeof(OVERLAPPED));
+				hEvent = CreateEvent(NULL, TRUE, FALSE, NULL);
+			}
+
+			virtual ~Op()
+			{
+				[[maybe_unused]] auto res = CloseHandle(hEvent);
+				assert(SUCCEEDED(res));
+			}
+
+			KIND kind;
+		};
+
+		struct CloseOp: Op
+		{
+			CloseOp()
+				: Op(KIND_CLOSE)
+			{}
+		};
+
+		void pushPendingOp(Unique<Op> op)
+		{
+			m_scheduledOperations.insert(op.get(), std::move(op));
+		}
+
+		Unique<Op> popPendingOp(Op* op)
+		{
+			auto it = m_scheduledOperations.lookup(op);
+			if (it == m_scheduledOperations.end())
+				return nullptr;
+			auto res = std::move(it->value);
+			m_scheduledOperations.remove(op);
+			return res;
+		}
+
 		Allocator* m_allocator = nullptr;
 		HANDLE m_completionPort = INVALID_HANDLE_VALUE;
+		Map<Op*, Unique<Op>> m_scheduledOperations;
 	public:
 		WinOSEventLoop(HANDLE completionPort, Allocator* allocator)
 			: m_allocator(allocator),
-			  m_completionPort(completionPort)
+			  m_completionPort(completionPort),
+			  m_scheduledOperations(allocator)
 		{}
+
+		~WinOSEventLoop() override
+		{
+			if (m_completionPort != INVALID_HANDLE_VALUE)
+			{
+				[[maybe_unused]] auto res = CloseHandle(m_completionPort);
+				assert(SUCCEEDED(res));
+			}
+		}
 
 		HumanError run() override
 		{
-			// TODO: implement function
-			return {};
+			while (true)
+			{
+				constexpr int MAX_ENTRIES = 32;
+				OVERLAPPED_ENTRY entries[MAX_ENTRIES];
+				ULONG numEntries = 0;
+				{
+					auto res = GetQueuedCompletionStatusEx(m_completionPort, entries, MAX_ENTRIES, &numEntries, INFINITE, FALSE);
+					if (res == FALSE)
+					{
+						auto error = GetLastError();
+						return errf(m_allocator, "Failed to get queued completion status: ErrorCode({})"_sv, error);
+					}
+				}
+
+				for (size_t i = 0; i < numEntries; ++i)
+				{
+					auto overlapped = entries[i].lpOverlapped;
+
+					auto op = popPendingOp((Op*)overlapped);
+					assert(op != nullptr);
+
+					switch (op->kind)
+					{
+					case Op::KIND_CLOSE:
+						m_scheduledOperations.clear();
+						return {};
+					default:
+						return errf(m_allocator, "unknown operation kind, {}"_sv, (int)op->kind);
+					}
+				}
+			}
 		}
 
 		void stop() override
 		{
-			// TODO: implement function
+			auto op = unique_from<CloseOp>(m_allocator);
+			[[maybe_unused]] auto res = PostQueuedCompletionStatus(m_completionPort, 0, NULL, (LPOVERLAPPED)op.get());
+			assert(SUCCEEDED(res));
+			pushPendingOp(std::move(op));
 		}
 	};
 
