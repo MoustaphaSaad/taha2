04e011f761a33d50aca73d80e0c25c9a2bd717ad
Mostafa Saad <moustapha.saad.abdelhamed@gmail.com>
2024-01-08 19:14:38 +0200

    handle closing clients in our echo example

--- examples/playground.cpp
+++ examples/playground.cpp
@@ -2,7 +2,7 @@
 #include <core/Log.h>
 #include <core/EventLoop.h>
 #include <core/Socket.h>
-#include <core/Array.h>
+#include <core/Hash.h>
 
 #include <signal.h>
 
@@ -16,24 +16,23 @@ void signalHandler(int signal)
 	}
 }
 
-class Echoer: public core::Reactor
+class EchoServer;
+
+class EchoClient: public core::Reactor
 {
 	core::Log* m_log = nullptr;
 	core::Unique<core::EventSource> m_eventSource;
+	EchoServer* m_server = nullptr;
 public:
-	explicit Echoer(core::Unique<core::EventSource> eventSource, core::Log* log)
+	explicit EchoClient(EchoServer* acceptor, core::Unique<core::EventSource> eventSource, core::Log* log)
 		: m_log(log),
-		  m_eventSource(std::move(eventSource))
+		  m_eventSource(std::move(eventSource)),
+		  m_server(acceptor)
 	{
 		(void)m_eventSource->eventLoop()->read(m_eventSource.get(), this);
 	}
 
-	void onRead(const core::ReadEvent* event) override
-	{
-		m_log->debug("read: \"{}\""_sv, core::StringView{event->buffer()});
-		(void)event->eventLoop()->write(event->source(), this, event->buffer());
-		(void)event->eventLoop()->read(event->source(), this);
-	}
+	void onRead(const core::ReadEvent* event) override;
 
 	void onWrite(const core::WriteEvent* event) override
 	{
@@ -41,17 +40,23 @@ public:
 	}
 };
 
-class Acceptor: public core::Reactor
+class EchoServer: public core::Reactor
 {
 	core::Allocator* m_allocator = nullptr;
 	core::Log* m_log = nullptr;
-	core::Array<core::Unique<Echoer>> m_clients;
+	core::Map<EchoClient*, core::Unique<EchoClient>> m_clients;
 public:
-	explicit Acceptor(core::Log* log, core::Allocator* allocator)
+	explicit EchoServer(core::Log* log, core::Allocator* allocator)
 		: m_allocator(allocator),
 		  m_log(log),
 		  m_clients(allocator)
 	{}
+
+	void clientDisconnected(EchoClient* client)
+	{
+		m_log->debug("disconnected"_sv);
+		m_clients.remove(client);
+	}
 
 	void onAccept(core::AcceptEvent* event) override
 	{
@@ -61,12 +66,27 @@ public:
 		m_log->debug("accepted socket: {}"_sv, socket->fd());
 
 		auto socketSource = event->eventLoop()->createEventSource(std::move(socket));
-		m_clients.push(unique_from<Echoer>(m_allocator, std::move(socketSource), m_log));
+		auto client = unique_from<EchoClient>(m_allocator, this, std::move(socketSource), m_log);
+		m_clients.insert(client.get(), std::move(client));
 
 		// reschedule accept
 		(void)event->eventLoop()->accept(event->source(), this);
 	}
 };
+
+void EchoClient::onRead(const core::ReadEvent* event)
+{
+	if (event->buffer().count() == 0)
+	{
+		m_server->clientDisconnected(this);
+	}
+	else
+	{
+		m_log->debug("read: \"{}\""_sv, core::StringView{event->buffer()});
+		(void) event->eventLoop()->write(event->source(), this, event->buffer());
+		(void) event->eventLoop()->read(event->source(), this);
+	}
+}
 
 int main()
 {
@@ -84,13 +104,13 @@ int main()
 	auto eventLoop = eventLoopResult.releaseValue();
 	EVENT_LOOP = eventLoop.get();
 
-	Acceptor acceptor{&log, &mallocator};
+	EchoServer echoServer{&log, &mallocator};
 
 	auto acceptSocket = core::Socket::open(&mallocator, core::Socket::FAMILY_IPV4, core::Socket::TYPE_TCP);
 	acceptSocket->bind("8080"_sv);
 	acceptSocket->listen();
 	auto acceptEventSocket = eventLoop->createEventSource(std::move(acceptSocket));
-	auto err = eventLoop->accept(acceptEventSocket.get(), &acceptor);
+	auto err = eventLoop->accept(acceptEventSocket.get(), &echoServer);
 	if (err)
 	{
 		log.critical("socket accept error, {}"_sv, err);
