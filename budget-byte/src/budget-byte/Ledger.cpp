a91d1ba98136a7dae2c19f434af7f53d43a4de21
Mostafa Saad <moustapha.saad.abdelhamed@gmail.com>
2024-03-22 21:52:08 +0200

    add db init sql code

--- budget-byte/src/budget-byte/Ledger.cpp
+++ budget-byte/src/budget-byte/Ledger.cpp
@@ -12,11 +12,48 @@ namespace budget
 	{
 		sqlite3* db = nullptr;
 
-		auto c_path = core::String{path, allocator};
-		auto rc = sqlite3_open_v2(c_path.data(), &db, DB_FLAG_READWRITE | DB_FLAG_CREATE | DB_FLAG_NOMUTEX | DB_FLAG_EXCLUSIVE, nullptr);
+		auto cPath = core::String{path, allocator};
+		auto rc = sqlite3_open_v2(cPath.data(), &db, DB_FLAG_READWRITE | DB_FLAG_CREATE | DB_FLAG_NOMUTEX | DB_FLAG_EXCLUSIVE, nullptr);
 		if (rc != SQLITE_OK)
 			return core::errf(allocator, "failed to open sqlite3 database, {}"_sv, sqlite3_errstr(rc));
 
-		return Ledger{db, std::move(c_path), allocator};
+		// setup the db file
+		const char* INIT_QUERY = R"QUERY(
+			PRAGMA journal_mode = WAL;
+			PRAGMA synchronous = NORMAL;
+			PRAGMA user_version = 1;
+
+			CREATE TABLE IF NOT EXISTS "accounts" (
+				id INTEGER PRIMARY KEY AUTOINCREMENT,
+				name TEXT UNIQUE NOT NULL,
+				balance INTEGER NOT NULL
+			);
+
+			CREATE TABLE IF NOT EXISTS "ledger" (
+				id INTEGER PRIMARY KEY AUTOINCREMENT,
+				amount INTEGER NOT NULL,
+				src_id INTEGER NOT NULL,
+				dst_id INTEGER NOT NULL,
+				date TEXT NOT NULL,
+				notes TEXT,
+				FOREIGN KEY(src_id) REFERENCES accounts(id),
+				FOREIGN KEY(dst_id) REFERENCES accounts(id)
+				CHECK (src_id <> dst_id)
+			);
+
+			INSERT OR IGNORE INTO accounts(name, balance) VALUES('init', 0);
+		)QUERY";
+
+		char* sqlerr = nullptr;
+		rc = sqlite3_exec(db, INIT_QUERY, nullptr, nullptr, &sqlerr);
+		if (rc != SQLITE_OK)
+		{
+			auto res = core::errf(allocator, "failed to init sqlite3 database, {}"_sv, sqlerr);
+			::free(sqlerr);
+			sqlite3_close_v2(db);
+			return res;
+		}
+
+		return Ledger{db, std::move(cPath), allocator};
 	}
 }
