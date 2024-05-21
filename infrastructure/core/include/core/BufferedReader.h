4f336774e28a88622d05dea7d423f13485b7fd81
Mostafa Saad <moustapha.saad.abdelhamed@gmail.com>
2024-05-21 22:20:23 +0300

    add BufferedReader type

---
+++ infrastructure/core/include/core/BufferedReader.h
@@ -0,0 +1,83 @@
+#pragma once
+
+#include "core/Stream.h"
+#include "core/Buffer.h"
+
+namespace core
+{
+	class BufferedReader: public Stream
+	{
+		Stream* m_source = nullptr;
+		Buffer m_buffer;
+		size_t m_readCursor = 0;
+	public:
+		BufferedReader(Stream* source, Allocator* allocator)
+			: m_source(source),
+			  m_buffer(allocator)
+		{}
+
+		Span<const std::byte> peek(size_t size)
+		{
+			auto availableSize = m_buffer.count() - m_readCursor;
+			if (availableSize < size)
+			{
+				auto requiredSize = size - availableSize;
+				auto offset = m_buffer.count();
+				m_buffer.resize(m_buffer.count() + requiredSize);
+
+				m_source->read(m_buffer.data() + offset, requiredSize);
+			}
+
+			availableSize = m_buffer.count() - m_readCursor;
+			auto resSize = size;
+			if (resSize > availableSize)
+				resSize = availableSize;
+			return Span<const std::byte>{m_buffer}.slice(m_readCursor, resSize);
+		}
+
+		size_t read(void* buffer, size_t size) override
+		{
+			auto remainingSize = size;
+			auto availableSize = m_buffer.count() - m_readCursor;
+			if (availableSize > 0)
+			{
+				auto readSize = size > availableSize ? availableSize : size;
+				memcpy(buffer, m_buffer.data() + m_readCursor, readSize);
+				remainingSize -= readSize;
+				m_readCursor += readSize;
+			}
+
+			if (m_readCursor == m_buffer.count())
+			{
+				m_buffer.clear();
+				m_readCursor = 0;
+			}
+
+			if (remainingSize > 0)
+			{
+				auto offset = size - remainingSize;
+				auto readSize = m_source->read((std::byte*)buffer + offset, remainingSize);
+				remainingSize -= readSize;
+			}
+			return size - remainingSize;
+		}
+
+		size_t write(const void* buffer, size_t size) override
+		{
+			coreUnreachable();
+			return 0;
+		}
+
+		int64_t seek(int64_t offset, SEEK_MODE whence) override
+		{
+			coreUnreachable();
+			return 0;
+		}
+
+		int64_t tell() override
+		{
+			coreUnreachable();
+			return 0;
+		}
+	};
+}
