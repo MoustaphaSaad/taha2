f3866d44a0d139400223f94d2b36fcf93a40279b
Mostafa Saad <moustapha.saad.abdelhamed@gmail.com>
2024-06-19 13:36:09 +0300

    vulkan-tutorial development environment

--- renderer/examples/vulkan-tutorial.cpp
+++ renderer/examples/vulkan-tutorial.cpp
@@ -1,7 +1,106 @@
 #include <core/File.h>
+#include <core/Log.h>
+#include <core/MiMallocator.h>
+
+#include <GLFW/glfw3.h>
+
+#define GLM_FORCE_RADIANS 1
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE 1
+#include <glm/vec4.hpp>
+#include <glm/mat4x4.hpp>
+
+#include <volk.h>
+
+namespace fmt
+{
+	template <> struct formatter<VkResult>
+	{
+		template <typename ParseContext> constexpr auto parse(ParseContext& ctx) { return ctx.begin(); }
+
+		template <typename FormatContext> auto format(VkResult r, FormatContext& ctx)
+		{
+			switch (r)
+			{
+			case VK_SUCCESS:
+				return fmt::format_to(ctx.out(), "VK_SUCCESS");
+			case VK_NOT_READY:
+				return fmt::format_to(ctx.out(), "VK_NOT_READY");
+			case VK_TIMEOUT:
+				return fmt::format_to(ctx.out(), "VK_TIMEOUT");
+			case VK_EVENT_SET:
+				return fmt::format_to(ctx.out(), "VK_EVENT_SET");
+			case VK_EVENT_RESET:
+				return fmt::format_to(ctx.out(), "VK_EVENT_RESET");
+			case VK_INCOMPLETE:
+				return fmt::format_to(ctx.out(), "VK_INCOMPLETE");
+			case VK_ERROR_OUT_OF_HOST_MEMORY:
+				return fmt::format_to(ctx.out(), "VK_ERROR_OUT_OF_HOST_MEMORY");
+			case VK_ERROR_OUT_OF_DEVICE_MEMORY:
+				return fmt::format_to(ctx.out(), "VK_ERROR_OUT_OF_DEVICE_MEMORY");
+			case VK_ERROR_INITIALIZATION_FAILED:
+				return fmt::format_to(ctx.out(), "VK_ERROR_INITIALIZATION_FAILED");
+			case VK_ERROR_DEVICE_LOST:
+				return fmt::format_to(ctx.out(), "VK_ERROR_DEVICE_LOST");
+			case VK_ERROR_MEMORY_MAP_FAILED:
+				return fmt::format_to(ctx.out(), "VK_ERROR_MEMORY_MAP_FAILED");
+			case VK_ERROR_LAYER_NOT_PRESENT:
+				return fmt::format_to(ctx.out(), "VK_ERROR_LAYER_NOT_PRESENT");
+			case VK_ERROR_EXTENSION_NOT_PRESENT:
+				return fmt::format_to(ctx.out(), "VK_ERROR_EXTENSION_NOT_PRESENT");
+			case VK_ERROR_FEATURE_NOT_PRESENT:
+				return fmt::format_to(ctx.out(), "VK_ERROR_FEATURE_NOT_PRESENT");
+			case VK_ERROR_INCOMPATIBLE_DRIVER:
+				return fmt::format_to(ctx.out(), "VK_ERROR_INCOMPATIBLE_DRIVER");
+			case VK_ERROR_TOO_MANY_OBJECTS:
+				return fmt::format_to(ctx.out(), "VK_ERROR_TOO_MANY_OBJECTS");
+			case VK_ERROR_FORMAT_NOT_SUPPORTED:
+				return fmt::format_to(ctx.out(), "VK_ERROR_FORMAT_NOT_SUPPORTED");
+			case VK_ERROR_FRAGMENTED_POOL:
+				return fmt::format_to(ctx.out(), "VK_ERROR_FRAGMENTED_POOL");
+			case VK_ERROR_UNKNOWN:
+			default:
+				return fmt::format_to(ctx.out(), "VK_ERROR_UNKNOWN");
+			}
+		}
+	};
+}
 
 int main()
 {
-	core::strf(core::File::STDOUT, "Hello, World!\n"_sv);
+	core::Mimallocator allocator{};
+	core::Log log{&allocator};
+
+	auto result = volkInitialize();
+	if (result != VK_SUCCESS)
+	{
+		log.critical("volkInitialize failed, ErrorCode({})"_sv, result);
+		return EXIT_FAILURE;
+	}
+	coreDefer { volkFinalize(); };
+
+	if (glfwInit() == GLFW_FALSE)
+	{
+		log.critical("glfwInit failed"_sv);
+		return EXIT_FAILURE;
+	}
+	coreDefer { glfwTerminate(); };
+
+	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+	auto window = glfwCreateWindow(800, 600, "Vulkan Window", nullptr, nullptr);
+	coreDefer { glfwDestroyWindow(window); };
+
+	uint32_t extensionCount = 0;
+	vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+	log.info("{} extensions supported"_sv, extensionCount);
+
+	glm::mat4 matrix;
+	glm::vec4 vec;
+	auto test = matrix * vec;
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+	}
+
 	return 0;
 }
