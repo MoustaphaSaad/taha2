240e0598fd878d84277b8c4f68f9b058289e8f47
Mostafa Saad <moustapha.saad.abdelhamed@gmail.com>
2024-09-22 22:00:58 +0200

    add sqlite statement initialization and queryVersion function

--- budget-app/ledger/include/ledger/Ledger.h
+++ budget-app/ledger/include/ledger/Ledger.h
@@ -14,9 +14,23 @@ namespace ledger
 		core::Allocator* m_allocator = nullptr;
 		core::String m_path;
 		sqlite3* m_db = nullptr;
+		sqlite3_stmt* m_getAppID = nullptr;
+		sqlite3_stmt* m_getUserVersion = nullptr;
 
 		void destroy()
 		{
+			if (m_getAppID)
+			{
+				sqlite3_finalize(m_getAppID);
+				m_getAppID = nullptr;
+			}
+
+			if (m_getUserVersion)
+			{
+				sqlite3_finalize(m_getUserVersion);
+				m_getUserVersion = nullptr;
+			}
+
 			if (m_db)
 			{
 				sqlite3_close(m_db);
@@ -35,7 +49,16 @@ namespace ledger
 		}
 
 		Ledger(sqlite3* db, core::String path, core::Allocator* allocator);
+
+		core::Result<sqlite3_stmt*> createStmt(core::StringView query);
+
 	public:
+		struct Version
+		{
+			int32_t appID;
+			int32_t userVersion;
+		};
+
 		LEDGER_EXPORT static core::Result<Ledger> open(core::StringView path, core::Allocator* allocator);
 
 		Ledger(const Ledger&) = delete;
@@ -58,5 +81,7 @@ namespace ledger
 		{
 			destroy();
 		}
+
+		LEDGER_EXPORT core::Result<Version> queryVersion();
 	};
 }
--- budget-app/ledger/src/ledger/Ledger.cpp
+++ budget-app/ledger/src/ledger/Ledger.cpp
@@ -8,6 +8,15 @@ namespace ledger
 		  m_db(db)
 	{}
 
+	core::Result<sqlite3_stmt*> Ledger::createStmt(core::StringView query)
+	{
+		sqlite3_stmt* res = nullptr;
+		auto rc = sqlite3_prepare_v3(m_db, query.data(), query.count(), 0, &res, nullptr);
+		if (rc != SQLITE_OK)
+			return core::errf(m_allocator, "sqlite3_prepare_v3 failed, {}"_sv, sqlite3_errstr(rc));
+		return res;
+	}
+
 	core::Result<Ledger> Ledger::open(core::StringView path, core::Allocator* allocator)
 	{
 		sqlite3* db = nullptr;
@@ -16,10 +25,56 @@ namespace ledger
 		auto rc = sqlite3_open_v2(cPath.data(), &db, SQLITE_OPEN_READWRITE | SQLITE_OPEN_CREATE | SQLITE_OPEN_NOMUTEX | SQLITE_OPEN_EXCLUSIVE, nullptr);
 		if (rc != SQLITE_OK)
 			return core::errf(allocator, "sqlite3_open_v2 failed, {}"_sv, sqlite3_errstr(rc));
-		coreDefer {sqlite3_close(db);};
+		auto ledger = Ledger{db, core::String{path, allocator}, allocator};
 
-		auto res = Ledger{db, core::String{path, allocator}, allocator};
-		db = nullptr;
-		return res;
+		struct SQLStmt
+		{
+			sqlite3_stmt*& stmt;
+			core::StringView query;
+		};
+
+		SQLStmt stmts[] = {
+			SQLStmt{ledger.m_getAppID, "PRAGMA application_id"_sv},
+			SQLStmt{ledger.m_getUserVersion, "PRAGMA user_version"_sv},
+		};
+
+		for (auto s: stmts)
+		{
+			auto res = ledger.createStmt(s.query);
+			if (res.isError()) return res.releaseError();
+			s.stmt = res.releaseValue();
+		}
+
+		return ledger;
+	}
+
+	core::Result<Ledger::Version> Ledger::queryVersion()
+	{
+		// get app id
+		auto rc = sqlite3_reset(m_getAppID);
+		if (rc != SQLITE_OK)
+			return core::errf(m_allocator, "sqlite3_reset failed, {}"_sv, sqlite3_errstr(rc));
+
+		rc = sqlite3_step(m_getAppID);
+		if (rc != SQLITE_DONE)
+			return core::errf(m_allocator, "sqlite3_step failed, {}"_sv, sqlite3_errstr(rc));
+
+		auto appID = sqlite3_column_int(m_getAppID, 0);
+
+		// get user version
+		rc = sqlite3_reset(m_getUserVersion);
+		if (rc != SQLITE_OK)
+			return core::errf(m_allocator, "sqlite3_reset failed, {}"_sv, sqlite3_errstr(rc));
+
+		rc = sqlite3_step(m_getUserVersion);
+		if (rc != SQLITE_DONE)
+			return core::errf(m_allocator, "sqlite3_step failed, {}"_sv, sqlite3_errstr(rc));
+
+		auto userVersion = sqlite3_column_int(m_getUserVersion, 0);
+
+		return Ledger::Version{
+			.appID = appID,
+			.userVersion = userVersion,
+		};
 	}
 }
